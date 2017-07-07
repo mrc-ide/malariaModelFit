b14837201dacabe8f5e6d491d80cfb5915e8542d
bobverity <r.verity@imperial.ac.uk>
2017-07-07 14:02:19 +0100

    improved plot_nice1

--- R/plot.R
+++ R/plot.R
@@ -2,43 +2,82 @@
 # -----------------------------------
 #' plot_nice1
 #'
-#' Simple plotting function for producing plot similar to base plot(), but in ggplot2. Has the advantage of being able to specify legends outside of plotting area more easily.
+#' Produces plot similar to base plot() function, but in ggplot2. Has the advantage of being able to specify legends outside of plotting area more easily. Multiple x, y and col arguments can be specified using lists of values.
 #'
 #' @export
 
 plot_nice1 <- function(x, y, col=1, xlab=NULL, ylab=NULL, main=NULL, pch=1, cex=1, legend=FALSE, legend_position="right", legend_title="legend_title", legend_label=NULL) {
     
-    # if no y variable then first argument is y
+    # if no y variable then first argument becomes y
     if (missing(y)) {
         y <- x
-        x <- 1:length(y)
+        if (is.list(y)) {
+            x <- mapply(function(x){1:length(x)}, y)
+        } else {
+            x <- 1:length(y)
+        }
     }
     
-    # set default axis names
-    if (is.null(xlab)) {
+    # try to set axis names automatically
+    if (is.null(xlab) && !is.list(x)) {
         xlab <- substitute(x)
     }
-    if (is.null(ylab)) {
+    if (is.null(ylab) && !is.list(y)) {
         ylab <- substitute(y)
     }
     
-    # if col is numeric then convert to default colours
-    if (is.numeric(col)) {
-        col <- (col-1)%%8+1	# recycle colours
-        col <- palette()[col]
+    # force y to list
+    if (!is.list(y)) {
+        y <- list(y)
     }
     
-    # create data frame from inputs
-    df1 <- data.frame(x=x, y=y, col=col)
+    # replicate x over list
+    if (!is.list(x)) {
+        x <- replicate(length(y), x, simplify=FALSE)
+    }
     
-    # extract levels from colour column (ensures correct ordering)
+    # replicate col over list
+    if (!is.list(col)) {
+        col <- replicate(length(y), col, simplify=FALSE)
+    }
+    
+    # check that all lists same length
+    if(!(length(x)==length(y) & length(x)==length(col))) {
+        stop("input lists of different lengths")
+    }
+    
+    # check that x and y inputs same length
+    if (!all(mapply(length,x)==mapply(length,y))) {
+        stop("x and y inputs must be corresponding lengths")
+    }
+    
+    # add each list element to data frame
+    df1 <- NULL
+    for (i in 1:length(x)) {
+        
+        # if col is numeric then convert to default colours
+        if (is.numeric(col[[i]])) {
+            col[[i]] <- (col[[i]]-1)%%8+1	# recycle colours
+            col[[i]] <- palette()[col[[i]]]
+        }
+        
+        # create data frame
+        col[[i]] <- rep(col[[i]], length(x[[i]]))
+        df1 <- rbind(df1, data.frame(x=x[[i]], y=y[[i]], col=col[[i]]))
+    }
+    
+    # get unique colour levels
     col_levels <- levels(df1$col)
     
     # create default legend labels
-    legend_labels <- paste0("legend_label", 1:length(col_levels))
+    if (is.null(legend_label)) {
+        legend_label <- paste0("legend_label", 1:length(col_levels))
+    }
+    
+    # --------------------
     
     # create plot and theme
-    plot1 <- ggplot(data=df1)
+    plot1 <- ggplot()
     plot1 <- plot1 + theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank())
     plot1 <- plot1 + theme(panel.background=element_blank())
     plot1 <- plot1 + theme(axis.line=element_line(colour="black", size=0.25))
@@ -52,7 +91,7 @@ plot_nice1 <- function(x, y, col=1, xlab=NULL, ylab=NULL, main=NULL, pch=1, cex=
     }
     
     # add points
-    plot1 <- plot1 + geom_point(aes(x, y, colour=col), shape=pch, size=2*cex)
+    plot1 <- plot1 + geom_point(aes(x, y, colour=col), shape=pch, size=2*cex, data=df1)
     
     # add titles
     plot1 <- plot1 + labs(x=xlab, y=ylab)
